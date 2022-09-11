b07b45b1af53b2a908a6e1b77191cc989c376d81
ayushp264@gmail.com <ayushp264@gmail.com>
2022-09-12 01:14:11 +0530

    12 Sep

---
+++ leetcode/src/TwoPointers/MiddleoftheLinkedList.java
@@ -0,0 +1,21 @@
+package TwoPointers;
+
+  public class ListNode {
+     int val;
+      public ListNode next;
+     ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+
+public class MiddleoftheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
